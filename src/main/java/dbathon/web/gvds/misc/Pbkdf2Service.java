6d7b99f4977e9a07fa6ca195098e4f481b91b5bb
Dominik Bathon <dominik.bathon@gmail.com>
2014-02-20 23:12:02 +0100

    add caching to Pbkdf2Service

--- src/main/java/dbathon/web/gvds/misc/Pbkdf2Service.java
+++ src/main/java/dbathon/web/gvds/misc/Pbkdf2Service.java
@@ -2,10 +2,16 @@ package dbathon.web.gvds.misc;
 
 import java.security.NoSuchAlgorithmException;
 import java.security.spec.InvalidKeySpecException;
+import java.util.concurrent.TimeUnit;
+import javax.annotation.PostConstruct;
 import javax.crypto.SecretKeyFactory;
 import javax.crypto.spec.PBEKeySpec;
 import javax.enterprise.context.ApplicationScoped;
 import com.google.common.base.Charsets;
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
+import com.google.common.collect.ImmutableList;
 
 @ApplicationScoped
 public class Pbkdf2Service {
@@ -15,10 +21,21 @@ public class Pbkdf2Service {
    */
   private static final int ITERATIONS = 8000;
 
-  /**
-   * TODO: maybe cache the hash for some time to reduce the overhead for every request
-   */
-  public byte[] hashPassword(String password, String salt) {
+  private LoadingCache<ImmutableList<String>, byte[]> cache;
+
+  @PostConstruct
+  private void postConstruct() {
+    cache =
+        CacheBuilder.newBuilder().expireAfterAccess(10, TimeUnit.MINUTES).maximumSize(1000).build(
+            new CacheLoader<ImmutableList<String>, byte[]>() {
+              @Override
+              public byte[] load(ImmutableList<String> key) throws Exception {
+                return hashPasswordInternal(key.get(0), key.get(1));
+              }
+            });
+  }
+
+  private byte[] hashPasswordInternal(String password, String salt) {
     final PBEKeySpec spec =
         new PBEKeySpec(password.toCharArray(), salt.getBytes(Charsets.UTF_8), ITERATIONS, 256);
 
@@ -29,6 +46,12 @@ public class Pbkdf2Service {
     catch (final NoSuchAlgorithmException | InvalidKeySpecException e) {
       throw new RuntimeException(e);
     }
+  }
+
+  public byte[] hashPassword(String password, String salt) {
+    final byte[] hash = cache.getUnchecked(ImmutableList.of(password, salt));
+    // clone the array to prevent external modification
+    return hash.clone();
   }
 
 }
